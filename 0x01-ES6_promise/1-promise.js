c9c5985ec688efbce4f89795b1cee8914d09f504
Akingbade675 <akingbadebasit26@gmail.com>
2023-04-19 12:57:41 +0000

    added 0x01-ES6_promise/1-promise.js file.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(new Error('The fake API is not working currently'));
+  });
+}
