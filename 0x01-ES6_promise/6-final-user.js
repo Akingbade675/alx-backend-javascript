a73f6cebe45ba0f68784a1b45317cc05ffaaa704
Akingbade675 <akingbadebasit26@gmail.com>
2023-04-19 15:30:52 +0000

    fixed bug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const result = [];
   const signUpUserPromise = signUpUser(firstName, lastName);
   return Promise.allSettled([signUpUserPromise, uploadPhoto(fileName)]).then((response) => {
+    const result = [];
     response.forEach((res) => {
-      result.push({ status: res.status, value: res.status === 'rejected' ? res.reason : res.value });
+      result.push({ status: res.status, value: res.status === 'rejected' ? `${res.reason}` : res.value });
     });
     return result;
   });
