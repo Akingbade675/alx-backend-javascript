c548553fa1413807f80b1211f297c78c65942118
Akingbade675 <akingbadebasit26@gmail.com>
2023-04-19 12:57:58 +0000

    added 0x01-ES6_promise/3-all.js file.

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  });
+}
