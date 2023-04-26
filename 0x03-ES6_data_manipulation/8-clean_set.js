666e2a396dad0bf06c7d3740648292373720d49a
Akingbade675 <akingbadebasit26@gmail.com>
2023-04-26 14:33:20 +0000

    fixed error in 8-clean_set.js file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
+/* eslint-disable consistent-return */
 export default function cleanSet(set, startString) {
   const output = [];
 
   set.forEach((element) => {
-    if (!startString || typeof startString !== 'string') return;
+    if (!startString || typeof startString !== 'string') return '';
     if (element.startsWith(startString)) output.push(element.slice(startString.length));
   });
 
